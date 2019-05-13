aeef02a379abe6baf91e7e91a454eb74fa0a0ad4
Gabriela de Farias Alexandre <gabriela.alexandre@betha.com.br>
2019-05-13 18:12:35 -0300

    ajustes

--- models/adote.js
+++ models/adote.js
@@ -18,8 +18,4 @@ const _model = new Schema({
     }]
 });
 
-_model.virtual('total').get(function() {
-    return this.itens ? this.itens.reduce((total, item) => total + (item.preco * item.quantidade), 0) : 0;
-})
-
 mongoose.model('adote', _model);
